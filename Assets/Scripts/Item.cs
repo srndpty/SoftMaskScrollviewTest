cfa1a82012134fe07a7981a0c6773169d79fff3a
Masaki Takano <takano@unbereal.co.jp>
2021-05-25 03:21:37 +0900

    commit project

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoftMaskScrollviewTest
+{
+	public class Item : MonoBehaviour
+	{
+		[SerializeField]
+		private Image image;
+
+		public void Setup()
+		{
+			//image.color = Random.ColorHSV();
+		}
+	}
+
+}
